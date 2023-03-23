5b4e9e1017ff665b71367a57c8db3fe1061cec6f
mehmetyasin-cirak <mehmetyasincirak@hotmail.com.tr>
2023-03-23 12:16:52 +0300

    Business katmanı düzenleme

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,29 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        ICarDal _iCarDal;
+
+        public CarManager(ICarDal iCarDal)
+        {
+            _iCarDal = iCarDal;
+        }
+
+        public List<Car> GetAll()
+        {
+            // İş kodları yazılacak.
+            return _iCarDal.GetAll();
+
+        }
+    }
+}
